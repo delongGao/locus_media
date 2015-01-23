eddea0d7b9bc15f77ea0f11a3635556e1da4bffc
Delong Gao <delong@tyemill.com>
2015-01-22 17:33:47 -0800

    complete likes

--- client/views/like/like.js
+++ client/views/like/like.js
@@ -1,20 +1,38 @@
 Template.like.helpers({
 	isLiked: function (postId) {
-		var likeRecord = Likes.find({ userId: Meteor.userId(), postId: postId});
-		if (likeRecord.count() > 0) {
+		var likeRecord = userLikePost(Meteor.userId(), postId);
+		if (likeRecord.like_or_not) {
 			return "fa-heart";
 		} else {
 			return "fa-heart-o";
 		}
 	}, 
 	printLikeId: function(postId) {
-		var likeRecord = Likes.find({ userId: Meteor.userId(), postId: postId});
-		if (likeRecord.count() > 0) {
-			return likeRecord.fetch()[0]._id;
+		var likeRecord = userLikePost(Meteor.userId(), postId);
+		if (likeRecord.like_or_not) {
+			return likeRecord.like_id;
 		} else {
 			return "";
 		}
+	},
+	shownLiker: function(postId) {
+		// display the default user for a post like
+		var numLikes = Likes.find({postId:postId}).count();
+		var likeRecord = userLikePost(Meteor.userId(), postId);
+		if (numLikes == 0) {
+			// no one likes the post
+			return "";
+		} else if (likeRecord.like_or_not) {
+			return "You and " + (numLikes - 1) + " others " ;
+		} else {
+			var lastLike = Likes.find({postId:postId},{sort: {createdAt: -1}, limit: 1}).fetch()[0];
+			var lastLiker = Meteor.users.findOne({_id: lastLike.userId});
+			return lastLiker.profile.name + " and " + (numLikes - 1) + " others " ;
+		}
 	}
+	// numLikes: function(postId) {
+	// 	return 
+	// }
 });
 
 Template.like.events({
@@ -35,10 +53,19 @@ Template.like.events({
 			Likes.insert({
 				userId: Meteor.userId(), 
 				postId: this.postId,
-				createdAt: new Date().valueOf()
+				createdAt: new Date()
 			}, function() {
 				likeIcon.removeClass( "fa-heart-o" ).addClass( "fa-heart" );
 			});
 		}
 	}
 });
+
+function userLikePost(userId, postId) {
+	var likeRecord = Likes.find({ userId: userId, postId: postId});
+	var result = {
+		"like_or_not": likeRecord.count() > 0,
+		"like_id" : likeRecord.count() > 0 ? likeRecord.fetch()[0]._id : null
+	}
+	return result;
+}
