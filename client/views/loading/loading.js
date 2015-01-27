e5646776911310809d9a0ed92f3087857336bff2
Delong Gao <delong@tyemill.com>
2015-01-26 17:28:25 -0800

    make environment variables secure

---
+++ client/views/loading/loading.js
@@ -0,0 +1,19 @@
+Template.loading.rendered = function () {
+	if ( ! Session.get('loadingSplash') ) {
+	    this.loading = window.pleaseWait({
+	    	logo: '/images/locus_spinner.gif',
+	    	backgroundColor: '#1abc9c',
+	    	loadingHtml: message + spinner
+	    });
+    	Session.set('loadingSplash', true); // just show loading splash once
+	}
+};
+
+Template.loading.destroyed = function () {
+	if ( this.loading ) {
+    	this.loading.finish();
+	}
+};
+
+var message = '<p class="loading-message">Loading, please wait...</p>';
+var spinner = '<div class="sk-spinner sk-spinner-rotating-plane"></div>';
---
+++ server/uploader.js
@@ -0,0 +1,5 @@
+S3.config = {
+    key: Meteor.settings.S3_KEY,
+    secret: Meteor.settings.S3_SECRET,
+    bucket: Meteor.settings.S3_BUCKET
+};
