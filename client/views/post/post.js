5a96fbf96670ea9620b922cf230164bdccf54b5d
Delong Gao <delong@tyemill.com>
2015-01-27 15:23:11 -0800

    modify tags

--- client/views/post/post.js
+++ client/views/post/post.js
@@ -4,6 +4,17 @@ Template.post.helpers({
 	},
 	tagObjects: function () {
     	var post_id = this._id;
+    	// var tmp_tags = this.tags;
+    	// // delete tmp_tags["general_tags"];
+    	// var special_tags = tmp_tags;
+    	// var general_tags = this.tags.general_tags;
+
+    	// var tagsArray = $.map(special_tags, function(key,value) { return {type: value, tag: key}; } ); 
+    	// // $.each(general_tags, function(key,value) {
+    	// // 	tagsArray.push({type: 'general tag', tag: value});
+    	// // });
+
+    	// // return tagsArray;
     	return _.map(this.tags.general_tags || [], function (tag) {
 	        return {post_id: post_id, tag: tag};
 	    });
@@ -36,9 +47,19 @@ Template.post.events({
 	},
     'click .fa-plus': function (evt, tmpl) {
     	var value = $(evt.target).siblings("input").val();
-    	var tagType = $('#tag-type-selector').val();
+    	var tagType = $(evt.target).siblings('#tag-type-selector').val();
 	    if (value.length > 0 && tagType.length > 0) {
-	    	Posts.update(this._id, {$addToSet: {'tags.general_tags': value}});
+	    	// add general tags
+	    	if (tagType === 'general') {
+	    		Posts.update(this._id, {$addToSet: {'tags.general_tags': value}});	
+	    	} 
+	    	// add t,s,c tags
+	    	else {
+	    		var temp_tag_obj = {};
+	    		temp_tag_obj['tags.' + tagType] = value;
+	    		Posts.update(this._id, {$set: temp_tag_obj});		
+	    	}
+	    	
 	        $(evt.target).siblings("input").val("");
 	    } else {
 	    	console.log("Tag cannot be empty!");
