56527b447831872db32d673a353d85eff2dc0901
Yan Annie Jiao <anniej@mit.edu>
2015-01-28 11:49:28 -0800

    search in popup

--- client/views/menu/menu.js
+++ client/views/menu/menu.js
@@ -2,4 +2,14 @@ Template.menu.rendered = function(){
   this.$('#userUploadNew').click(function(){
   	$('#uploadDimmer').dimmer('show');
   });
+
+  this.$('#goSearch').popup({
+    popup: $('.custom.popup'),
+    on: 'click'
+  })
 }
+
+// Template.menu.events({
+//   'click #goSearch': function () {
+//     $('')
+// });
