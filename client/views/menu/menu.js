0de38c65cabc89a94e346eac6939fc18c83d4d5c
Yan Annie Jiao <anniej@mit.edu>
2015-01-28 11:09:12 -0800

    Revert d676f55..0d6bea5
    This rolls back to commit d676f55ee3f4129599c8be2d24d58b903991b774.

--- client/views/menu/menu.js
+++ client/views/menu/menu.js
@@ -2,10 +2,4 @@ Template.menu.rendered = function(){
   this.$('#userUploadNew').click(function(){
   	$('#uploadDimmer').dimmer('show');
   });
-
-  this.$('#goSearch')
-  .popup({
-    inline   : true,
-    position : 'bottom left',
-  });
 }
